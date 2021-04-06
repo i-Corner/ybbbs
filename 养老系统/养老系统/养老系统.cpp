af2f39d18b19f15ae23acfd2849e958108b97f8b
Ghost <1109434570@qq.com>
2021-04-06 21:53:08 +0800

    !

---
+++ 养老系统/养老系统/养老系统.cpp
@@ -0,0 +1,10 @@
+﻿#include<bits/stdc++.h>
+typedef int Status;
+typedef int SElemType;
+typedef int QElemType;
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INEASIRLE -1
+#define OVERFLOW -2
